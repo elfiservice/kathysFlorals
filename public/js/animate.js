adedae79aec602894339586327b05a47df672f2e
Megmalia <megmalia9@gmail.com>
2018-04-05 07:02:58 -0700

    fadeInWIP

--- public/js/animate.js
+++ public/js/animate.js
@@ -1,5 +1,8 @@
-$('.movement').waypoint(function(direction) {
-    $('.movement').addClass('animated fadeInUp');
+//Animations on Scroll
+
+
+$('.feature1').waypoint(function(direction) {
+    $('.feature1').addClass('animated fadeIn');
 }, {
-    offset: '50%'
+    offset: '50%' //make the animation happen a little earlier
 });
