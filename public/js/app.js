35abf82e9f237ec5dedb4c4e97a6becabc32e97b
Megmalia <megmalia9@gmail.com>
2018-04-05 22:56:37 -0700

    updateModal

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,12 @@
 // Get the modal
 var modal = document.getElementByClass('modal');
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementsByClassName('.myImg');
-var modalImg = document.getElementsByClassName("#img01");
-var captionText = document.getElementsByClassName(".caption");
+// Get the image and insert it inside the modal - 
+//
+//use its "alt" text as a caption
+var img = document.getElementsByClassName('myImg');
+var modalImg = document.getElementsByTag("img");
+var captionText = document.getElementsByClassName("caption");
 img.onclick = function() {
     modal.style.display = "block";
     modalImg.src = this.src;
