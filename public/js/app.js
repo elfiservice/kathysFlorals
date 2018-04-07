b3bf6d647b833b4d2d1d7911f3b2df2029ea9b77
Armando Jr <elfiservice@hotmail.com>
2018-04-06 21:31:27 -0300

    refactore: my changes

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,33 @@
 // Get the modal
-var modal = document.getElementByClass('modal');
+var modal = document.getElementById('myModal');
+modal.style.display = "none";
+
+var modalImg = document.getElementById("imgModal");
+var captionText = document.getElementById("caption");
 
 // Get the image and insert it inside the modal - 
 //
-//use its "alt" text as a caption
-var img = document.getElementsByClassName('myImg');
-var modalImg = document.getElementsByTag("img");
-var captionText = document.getElementsByClassName("caption");
-img.onclick = function() {
+let worbookContainer = document.getElementById("portfolio");
+
+worbookContainer.onclick = function(cb) {
+    console.log(cb.target.src);
     modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-};
+    modalImg.src = cb.target.src;
+    captionText.innerHTML = cb.target.alt;
+    
+}
+
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var closeBtn = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
+closeBtn.onclick = function() {
+   modal.style.display = "none";
+    let modalBack = document.querySelectorAll(".modal-backdrop.in");
+    modalBack[0].remove();
+    modalBack[1].remove();
+    modal.classList.remove("in");
+    document.body.classList.remove("modal-open");
+    
 };
